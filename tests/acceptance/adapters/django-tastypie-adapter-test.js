af628734c4c758d07cc3fc2ad623e685351aeaa7
Pedro Kiefer <pedro@kiefer.com.br>
2015-02-02 22:57:17 -0200

    Move most unit tests to acceptance tests

--- tests/acceptance/adapters/django-tastypie-adapter-test.js
+++ tests/acceptance/adapters/django-tastypie-adapter-test.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 import startApp from '../../helpers/start-app';
+import Person from '../../../models/person';
+import Group from '../../../models/group';
+import Post from '../../../models/post';
+import Comment from '../../../models/comment';
+import DjangoTastypieSerializer from 'ember-data-tastypie-adapter/serializers/dta';
 
 var application;
 var store;
@@ -348,5 +353,254 @@ test("metadata is accessible", function () {
           2,
           "Metadata can be accessed with metadataFor");
     });
+  });
+});
+
+test("adding hasMany relationships parses the Resource URI (default key)", function() {
+
+  Person.reopen({
+    name: DS.attr('string'),
+    group: DS.belongsTo('group', { async: true })
+  });
+
+  Group.reopen({
+    people: DS.hasMany('person', { async: true })
+  });
+
+  run(function () {
+    store.push('person', {id: 1, name: "Maurice Moss"});
+    store.push('person', {id: 2, name: "Roy"});
+    store.push('group', {id: 1, name: "Team"});
+  });
+
+  run(function () {
+    Ember.RSVP.hash({
+      moss: store.find('person', 1),
+      roy: store.find('person', 2),
+      group: store.find('group', 1)
+    }).then(function (objects) {
+      var group = objects.group,
+          people;
+
+      return group.get('people').then(function (people) {
+        people.pushObject(objects.moss);
+        people.pushObject(objects.roy);
+
+        ajaxResponse();
+        return group.save();
+      });
+    }).
+    then(function (data) {
+      expectUrl('/api/v1/group/1/', 'modify Group URL');
+      expectType("PUT");
+      expectData({name: "Team", people: ['/api/v1/person/1/', '/api/v1/person/2/']});
+
+      return store.find('person', 2);
+    }).then(function (person) {
+      equal(person.get('name'), 'Roy');
+      equal(person.get('group').get('name'), 'Team');
+    });
+  });
+});
+
+test("async hasMany always returns a promise", function() {
+
+  Post.reopen({
+    comments: DS.hasMany('comment', { async: true })
+  });
+  adapter.set('coalesceFindRequests', true);
+
+  run(function() {
+    store.push('post', { id: 1, text: "Some text", comments: ['1', '2']});
+
+    store.find('post', 1).then(function(post) {
+      ajaxResponse({
+        objects: [
+          { id: 1, text: "Rein Heinrichs", resource_uri: '/api/v1/comment/1/' },
+          { id: 2, text: "Tom Dale", resource_uri: '/api/v1/comment/2/' }
+        ]
+      });
+      ok(post.get('comments') instanceof DS.PromiseArray, "comments is a promise");
+    });
+  });
+
+});
+
+test("sync hasMany find with full=True", function() {
+
+  Post.reopen({
+    comments: DS.hasMany('comment', { async: false })
+  });
+
+  Comment.reopen({
+    post: DS.belongsTo('post')
+  });
+
+  ajaxResponse({
+    id: 1,
+    text: "Some Text",
+    comments: [
+      {id: 1, text: 'Comment 1', post: '/api/v1/post/1/', resource_uri: '/api/v1/comment/1/'},
+      {id: 2, text: 'Comment 2', post: '/api/v1/post/1/', resource_uri: '/api/v1/comment/2/'}
+    ],
+    resource_uri: '/api/v1/post/1/'
+  });
+
+  run(function() {
+    store.find('post', 1).then(function(post) {
+      ok(post.get('comments') instanceof DS.ManyArray, "comments is not a promise");
+      ok(post.get('comments').isLoaded, "Comments are loaded");
+      equal(post.get('text'), "Some Text", "the post has the correct data");
+    });
+  });
+});
+
+test("sync hasMany save should not need to resolve relationship", function() {
+
+  Post.reopen({
+    comments: DS.hasMany('comment', { async: false })
+  });
+
+  Comment.reopen({
+    post: DS.belongsTo('post', { async: false })
+  });
+
+  var post;
+  run(function() {
+    post = store.push('post', { id: 1, text: "Some text", comments: [1, 2]});
+    store.push('comment', {id: 1, text: "Comment 1", post: post});
+    store.push('comment', {id: 2, text: "Comment 2", post: post});
+  });
+
+  adapter.findMany = function() {
+    ok(false, "Should not get here.");
+  };
+
+  run(function() {
+    store.find('post', 1).then(function(post) {
+      post.set('text', 'New Text');
+
+      equal(post.get('isDirty'), true, "the post dirty");
+
+      ajaxResponse();
+      return post.save();
+    }).then(function(post) {
+      expectUrl('/api/v1/post/1/', 'modify Group URL');
+      expectType("PUT");
+      expectData({text: "New Text", comments: [
+        { id: "1", text: 'Comment 1', post: '/api/v1/post/1/' },
+        { id: "2", text: 'Comment 2', post: '/api/v1/post/1/' }
+      ]});
+
+      equal(post.get('isDirty'), false, "the post is not dirty anymore");
+      equal(post.get('text'), "New Text", "the post was updated");
+    });
+  });
+
+});
+
+test("async hasMany save should resolve promise before post", function() {
+
+  Post.reopen({
+    comments: DS.hasMany('comment', { async: true })
+  });
+
+  Comment.reopen({
+    post: DS.belongsTo('post', { async: true })
+  });
+  adapter.set('coalesceFindRequests', true);
+
+  run(function() {
+    store.push('post', { id: 1, text: "Some text", comments: [1, 2]});
+  });
+
+  var count = 0;
+  adapter.findMany = function() {
+    ok(count++ === 0, "findHasMany called once");
+
+    return Ember.RSVP.resolve({ objects: [
+      {id: 1, text: "Comment 1", post: '/api/v1/post/1/', resource_uri: '/api/v1/comment/1/'},
+      {id: 2, text: "Comment 2", post: '/api/v1/post/1/', resource_uri: '/api/v1/comment/2/'}
+    ]});
+  };
+
+  run(function() {
+    store.find('post', 1).then(function(post) {
+      post.set('text', 'New Text');
+
+      equal(post.get('isDirty'), true, "the post dirty");
+
+      ajaxResponse();
+      return post.save();
+    }).then(function(post) {
+      expectUrl('/api/v1/post/1/', 'modify Group URL');
+      expectType("PUT");
+      expectData({text: "New Text", comments: ['/api/v1/comment/1/', '/api/v1/comment/2/'] });
+
+      equal(post.get('isDirty'), false, "the post is not dirty anymore");
+      equal(post.get('text'), "New Text", "the post was updated");
+    });
+  });
+});
+
+test("findQuery - payload 'meta' is accessible on the record array", function() {
+  ajaxResponse({
+    meta: { offset: 5 },
+    objects: [{id: 1, name: "Roy"}]
+  });
+
+  run(function() {
+    store.findQuery('person', { page: 2 }).then(function(people) {
+      equal(
+          people.get('meta.offset'),
+          5,
+          "Reponse metadata can be accessed with recordArray.meta"
+      );
+    });
+  });
+});
+
+test("findQuery - each record array can have it's own meta object", function() {
+  ajaxResponse({
+    meta: { offset: 5 },
+    objects: [{id: 1, name: "Roy"}]
+  });
+
+  run(function() {
+    store.findQuery('person', { page: 2 }).then(function(people) {
+      equal(
+          people.get('meta.offset'),
+          5,
+          "Reponse metadata can be accessed with recordArray.meta"
+      );
+      ajaxResponse({
+        meta: { offset: 1 },
+        objects: [{id: 1, name: "Maurice Moss"}]
+      });
+      store.findQuery('person', { page: 1}).then(function(newPeople){
+        equal(newPeople.get('meta.offset'), 1, 'new array has correct metadata');
+        equal(people.get('meta.offset'), 5, 'metadata on the old array hasnt been clobbered');
+      });
+    });
+  });
+});
+
+test("the adapter can use custom keys", function() {
+  application.__container__.register('serializer:person', DjangoTastypieSerializer.extend({
+    attrs: { name: 'name_custom' }
+  }));
+
+  ajaxResponse({ objects: [{ id: 1, name_custom: "Rails is omakase", resource_uri: '/api/v1/person/1/' }, { id: 2, name_custom: "The Parley Letter", resource_uri: '/api/v1/person/2/' }] });
+
+  store.findAll('person').then(function(people) {
+    var person1 = store.getById('person', 1),
+        person2 = store.getById('person', 2);
+
+    deepEqual(person1.getProperties('id', 'name'), { id: "1", name: "Rails is omakase" }, "Person 1 is loaded");
+    deepEqual(person2.getProperties('id', 'name'), { id: "2", name: "The Parley Letter" }, "Person 2 is loaded");
+
+    equal(people.get('length'), 2, "The posts are in the array");
+    equal(people.get('isLoaded'), true, "The RecordArray is loaded");
+    deepEqual(people.toArray(), [ person1, person2 ], "The correct records are in the array");
   });
 });
